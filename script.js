5d18b9a421f22dac1d93ef0d38187138096ba674
Vitaly Lebedkov <lebedkov.2000@mail.ru>
2024-10-01 19:28:28 +0700

    Калькулятор, версия 2

--- script.js
+++ script.js
@@ -1,112 +1,85 @@
-let number1 = document.getElementById('1')
-let number2 = document.getElementById('2')
-let number3 = document.getElementById('3')
-let number4 = document.getElementById('4')
-let number5 = document.getElementById('5')
-let number6 = document.getElementById('6')
-let number7 = document.getElementById('7')
-let number8 = document.getElementById('8')
-let number9 = document.getElementById('9')
-let number0 = document.getElementById('zero')
-let ac = document.getElementById('ac')
-let sqrt = document.getElementById('sqrt')
-let divide = document.getElementById('divide')
-let percent = document.getElementById('percent')
-let multuply = document.getElementById('umnozhit')
-let plus = document.getElementById('pribavit')
-let minus = document.getElementById('otnyat')
-let equal = document.getElementById('ravno')
-let deleteSmt = document.getElementById('delete')
-let dott = document.getElementById('dott')
+// 1. Формируем калькулятор при помощи массива
+const allButtons = ['AC', '√', '/', '%', '7', '8', '9', '*', '4', '5', '6', '+', '1', '2', '3', '-', '0', '.', '='];
+allButtons.map(el => document.getElementById('buttons').insertAdjacentHTML('beforeend', `<button value='${el}'>${el}</button>`));
+Array.from(document.getElementById('buttons').children).forEach(el => el.classList.add('button'));
+// Конец
 
-let result = document.getElementById('result')
-let input = document.getElementById('input')
+// 2. Вводим необходимые для работы переменные
+let a = '';
+let b = '';
+let sign = '';
+let allNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+let allSigns = ['√', '/', '%', '*', '+', '-'];
+let result = '';
+// Конец
 
-let firstOperand = ''
-let secondOperand = ''
-let sign = ''
-let finalResult = ''
-let helper = ''
 
-let allNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-let allSigns = ['√', '/', '%', '*', '+', '-']
-
-document.querySelector('.buttons__grid').onclick = function(click) {
-    if(!click.target.classList.contains('button')) {
+// 3. Вся логика калькулятора
+document.getElementById('buttons').onclick = function (click) {
+    if (!click.target.classList.contains('button')) {
         return
-    }
+    };
 
-    if (click.target.classList.contains('ac')) {
-        firstOperand = ''
-        secondOperand = ''
+    // Логика операнд и знаков
+    if (b === '' && sign === '' && allNumbers.includes(click.target.textContent)) {
+        a += click.target.textContent
+        document.getElementById('result').innerHTML = a
+    } else if (a !== '' && allSigns.includes(click.target.textContent)) {
+        sign = click.target.textContent
+        document.getElementById('result').innerHTML = sign
+    } else if (a !== '' && sign !== '' && result === '' && allNumbers.includes(click.target.textContent)) {
+        b += click.target.textContent
+        document.getElementById('result').innerHTML = b
+    } else if (result !== '' && allNumbers.includes(click.target.textContent)) {
+        a = result
+        result = ''
+        if (result === '') {
+            b = ''
+            b += click.target.textContent
+            document.getElementById('result').innerHTML = b
+        }
+    };
+
+    // AC - сброс введённых параметров
+    if (click.target.textContent === 'AC') {
+        document.getElementById('result').innerHTML = ''
+        a = ''
+        b = ''
         sign = ''
-        input.innerHTML = 0
-        result.innerHTML = 0
-        finalResult = ''
-    }
+        result = ''
+    };
 
-    let clickAnyButton = click.target.textContent
-
-    if (allNumbers.includes(clickAnyButton)) {
-        if(secondOperand === '' && sign === '') {
-            firstOperand += clickAnyButton
-            input.innerHTML = firstOperand
-        } else if(finalResult !== '') {
-            firstOperand = finalResult
-            finalResult = ''
-            if(finalResult === '') {
-                secondOperand = ''
-            }
-            secondOperand += clickAnyButton
-            input.innerHTML = secondOperand
-        } else {
-            secondOperand += clickAnyButton
-            input.innerHTML = secondOperand
-        }
-    }
-
-    if(allSigns.includes(clickAnyButton)) {
-        sign = clickAnyButton
-        input.innerHTML = sign
-    }
-
-    if(click.target.classList.contains('equal')) {
-        if(secondOperand === '') {
-            secondOperand = firstOperand
-        }
-        switch(sign) {
+    // Счёт
+    if (click.target.textContent === '=') {
+        switch (sign) {
             case '+':
-                finalResult = Number(firstOperand) + Number(secondOperand)
-                result.innerHTML = finalResult
+                document.getElementById('result').innerHTML = Number(a) + Number(b)
+                result = Number(a) + Number(b)
                 break
             case '-':
-                finalResult = Number(firstOperand) - Number(secondOperand)
-                result.innerHTML = finalResult
+                document.getElementById('result').innerHTML = Number(a) - Number(b)
+                result = Number(a) - Number(b)
                 break
             case '*':
-                finalResult = Number(firstOperand) * Number(secondOperand)
-                result.innerHTML = finalResult
+                document.getElementById('result').innerHTML = Number(a) * Number(b)
+                result = Number(a) * Number(b)
                 break
             case '/':
-                if (secondOperand === '0') {
-                    result.innerHTML = 'На ноль делить нельзя'
-                    firstOperand = ''
-                    secondOperand = ''
-                    finalResult = ''
-                    return
+                if (b === '0') {
+                    document.getElementById('result').innerHTML = 'На ноль делить нельзя'
+                    a = ''
+                    b = ''
+                    sign = ''
                 } else {
-                    finalResult = Number(firstOperand) / Number(secondOperand)
-                    result.innerHTML = finalResult
-                    break
+                    document.getElementById('result').innerHTML = Number(a) / Number(b)
+                    result = Number(a) / Number(b)
                 }
+                break
             case '√':
-                finalResult = Math.sqrt(Number(firstOperand))
-                result.innerHTML = finalResult
+                document.getElementById('result').innerHTML = Math.sqrt(a)
+                result = Number(a) * Number(b)
                 break
         }
-        input.innerHTML = 0
     }
-
-
-    console.log(firstOperand, secondOperand, sign, finalResult)
+    console.log(a, sign, b, result)
 }
